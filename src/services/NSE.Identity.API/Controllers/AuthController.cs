1842a4042f093bfc6b82c9a795019fad9d671adb
Joanarfc <joanarfcarvalho@hotmail.com>
2023-02-13 19:30:33 -0300

    Added the method GerarJwt to the AuthController

--- src/services/NSE.Identity.API/Controllers/AuthController.cs
+++ src/services/NSE.Identity.API/Controllers/AuthController.cs
@@ -1,7 +1,15 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using NSE.Identity.API.Extensions;
 using NSE.Identity.API.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
 
 namespace NSE.Identity.API.Controllers
 {
@@ -11,12 +19,15 @@ namespace NSE.Identity.API.Controllers
     {
         private readonly SignInManager<IdentityUser> _signInManager;
         private readonly UserManager<IdentityUser> _userManager;
+        private readonly AppSettings _appSettings;
 
         public AuthController(SignInManager<IdentityUser> signInManager,
-                              UserManager<IdentityUser> userManager)
+                              UserManager<IdentityUser> userManager,
+                              IOptions<AppSettings> appSettings)
         {
             _signInManager = signInManager;
             _userManager = userManager;
+            _appSettings = appSettings.Value;
         }
 
         [HttpPost("nova-conta")]
@@ -36,7 +47,7 @@ namespace NSE.Identity.API.Controllers
             if (result.Succeeded)
             {
                 await _signInManager.SignInAsync(user, isPersistent: false);
-                return Ok();
+                return Ok(await GerarJwt(usuarioRegisto.Email));
             }
 
             return BadRequest();
@@ -51,9 +62,72 @@ namespace NSE.Identity.API.Controllers
 
             if (result.Succeeded)
             {
-                return Ok();
+                return Ok(await GerarJwt(usuarioLogin.Email));
             }
             return BadRequest();
         }
+        private async Task<UsuarioRespostaLogin> GerarJwt(string email)
+        {
+            /* Retrieve the user information (claims and roles) associated with the email using the _userManager  */
+            var user = await _userManager.FindByNameAsync(email);
+            var claims = await _userManager.GetClaimsAsync(user);
+            var userRoles = await _userManager.GetRolesAsync(user);
+
+            /* Add claims with additional information: user Id (Sub), Email, JWT unique identifier (Jti)
+               and the time of token issuance (NBF and IAT) */
+            claims.Add(new Claim(JwtRegisteredClaimNames.Sub, user.Id));
+            claims.Add(new Claim(JwtRegisteredClaimNames.Email, user.Email));
+            claims.Add(new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()));
+            claims.Add(new Claim(JwtRegisteredClaimNames.Nbf, ToUnixEpochDate(DateTime.UtcNow).ToString()));
+            claims.Add(new Claim(JwtRegisteredClaimNames.Iat, ToUnixEpochDate(DateTime.UtcNow).ToString(), ClaimValueTypes.Integer64));
+
+            foreach (var userRole in userRoles)
+            {
+                claims.Add(new Claim("role", userRole));
+            }
+
+            var identityClaims = new ClaimsIdentity();
+            identityClaims.AddClaims(claims);
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+
+            /* JWT generation based on:
+               the values we defined in the appsettings.json file for the Emissor (Issuer), ValidoEm (Audience) and ExpiracaoHoras (Expires)
+               the ClaimsIdentity object (Subject)
+               and the security key (SigningCredentials) */
+            var token = tokenHandler.CreateToken(new SecurityTokenDescriptor
+            {
+                Issuer = _appSettings.Emissor,
+                Audience = _appSettings.ValidoEm,
+                Subject = identityClaims,
+                Expires = DateTime.UtcNow.AddHours(_appSettings.ExpiracaoHoras),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            });
+
+            /* Generate the encoded token */
+            var endodedToken = tokenHandler.WriteToken(token);
+
+            /* Generation of a response object containing the encoded token, its expiration time
+               and the user information (Id, email and Claims) associated with the token */
+            var response = new UsuarioRespostaLogin
+            {
+                AccessToken = endodedToken,
+                ExpiresIn = TimeSpan.FromHours(_appSettings.ExpiracaoHoras).TotalSeconds,
+                UsuarioToken = new UsuarioToken
+                {
+                    Id = user.Id,
+                    Email = user.Email,
+                    Claims = claims.Select(c => new UsuarioClaim { Type = c.Type, Value = c.Value })
+                }
+            };
+
+            return response;
+        }
+
+        /* Calculate the number of seconds elapsed since the Unix epoch (January 1, 1970, 00:00:00 UTC) to a given date */
+        private static long ToUnixEpochDate(DateTime date)
+        => (long)Math.Round((date.ToUniversalTime() - new DateTimeOffset(1970, 1, 1, 0, 0, 0,
+              TimeSpan.Zero)).TotalSeconds);
     }
 }
