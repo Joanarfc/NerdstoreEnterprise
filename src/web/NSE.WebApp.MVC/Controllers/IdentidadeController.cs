57f9ac0a7fcc7f96b2094d2fa80b23351b259ff2
Joanarfc <joanarfcarvalho@hotmail.com>
2023-02-13 23:27:31 -0300

    Added the Models, Views and Controller for user Login and Registration

---
+++ src/web/NSE.WebApp.MVC/Controllers/IdentidadeController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NSE.WebApp.MVC.Models;
+using System.Threading.Tasks;
+
+namespace NSE.WebApp.MVC.Controllers
+{
+    public class IdentidadeController : Controller
+    {
+        [HttpGet]
+        [Route("nova-conta")]
+        public IActionResult Registo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("nova-conta")]
+        public async Task<IActionResult> Registo(UsuarioRegisto usuarioRegisto)
+        {
+            if(!ModelState.IsValid) return View(usuarioRegisto);
+
+            // API - User registration
+
+            if (false) return View(usuarioRegisto);
+
+            // Execute login in the app
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        [Route("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login(UsuarioLogin usuarioLogin)
+        {
+            if (!ModelState.IsValid) return View(usuarioLogin);
+
+            // API - User login
+
+            if (false) return View(usuarioLogin);
+
+            // Execute login in the app
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        [Route("sair")]
+        public IActionResult Logout()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
