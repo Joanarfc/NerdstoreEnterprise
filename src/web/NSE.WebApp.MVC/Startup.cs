e87ec6a5e013dff843341db4b01968348f8ae5e4
Joanarfc <joanarfcarvalho@hotmail.com>
2023-02-13 22:34:03 -0300

    Refactoration of the MVC configuration code into a separate file, WebAppConfig.cs

--- src/web/NSE.WebApp.MVC/Startup.cs
+++ src/web/NSE.WebApp.MVC/Startup.cs
@@ -1,14 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using NSE.WebApp.MVC.Configuration;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace NSE.WebApp.MVC
 {
@@ -24,35 +19,13 @@ namespace NSE.WebApp.MVC
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddAuthenticationConfiguration();
-            services.AddControllersWithViews();
+
+            services.AddMvcConfiguration();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseAuthenticationConfiguration();
-
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();
-            }
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-            });
+            app.UseMvcConfiguration(env);
         }
     }
 }
