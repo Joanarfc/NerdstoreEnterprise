cdba06339239374c33a4ecf5551ec14d0aeb27c0
Joanarfc <joanarfcarvalho@hotmail.com>
2023-02-14 15:54:14 -0300

    Improved the code to set the HttpClient base address as the Identity API Url from appsettings

--- src/web/NSE.WebApp.MVC/Services/IAutenticacaoService.cs
+++ src/web/NSE.WebApp.MVC/Services/IAutenticacaoService.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Options;
 using NSE.WebApp.MVC.Extensions;
 using NSE.WebApp.MVC.Models;
+using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -15,19 +16,18 @@ namespace NSE.WebApp.MVC.Services
     public class AutenticacaoService : Service, IAutenticacaoService
     {
         private readonly HttpClient _httpClient;
-        private readonly AppSettings _settings;
 
         public AutenticacaoService(HttpClient httpClient, 
                                   IOptions<AppSettings> settings)
         {
+            httpClient.BaseAddress = new Uri(settings.Value.AutenticacaoUrl);
             _httpClient = httpClient;
-            _settings = settings.Value;
         }
         public async Task<UsuarioRespostaLogin> Registo(UsuarioRegisto usuarioRegisto)
         {
             var loginContent = ObterConteudo(usuarioRegisto);
 
-            var response = await _httpClient.PostAsync($"{_settings.AutenticacaoUrl}/api/identidade/nova-conta", loginContent);
+            var response = await _httpClient.PostAsync("/api/identidade/nova-conta", loginContent);
 
             if (!TratarErrosResponse(response))
             {
@@ -43,7 +43,7 @@ namespace NSE.WebApp.MVC.Services
         {
             var loginContent = ObterConteudo(usuarioLogin);
 
-            var response = await _httpClient.PostAsync($"{_settings.AutenticacaoUrl}/api/identidade/autenticar", loginContent);
+            var response = await _httpClient.PostAsync("/api/identidade/autenticar", loginContent);
 
             if (!TratarErrosResponse(response))
             {
