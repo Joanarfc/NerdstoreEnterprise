bb57b14da1ce1910d1b5c383dd2866abfe27b3f7
Joanarfc <joanarfcarvalho@hotmail.com>
2023-02-14 15:26:58 -0300

    Code refactoration for serialization and deserialization

--- src/web/NSE.WebApp.MVC/Services/IAutenticacaoService.cs
+++ src/web/NSE.WebApp.MVC/Services/IAutenticacaoService.cs
@@ -1,7 +1,5 @@
 ﻿using NSE.WebApp.MVC.Models;
 using System.Net.Http;
-using System.Text;
-using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace NSE.WebApp.MVC.Services
@@ -22,47 +20,36 @@ namespace NSE.WebApp.MVC.Services
         }
         public async Task<UsuarioRespostaLogin> Registo(UsuarioRegisto usuarioRegisto)
         {
-            var loginContent = new StringContent(JsonSerializer.Serialize(usuarioRegisto), Encoding.UTF8, "application/json");
+            var loginContent = ObterConteudo(usuarioRegisto);
 
             var response = await _httpClient.PostAsync("https://localhost:44363/api/identidade/nova-conta", loginContent);
 
-            var options = new JsonSerializerOptions
-            {
-                PropertyNameCaseInsensitive = true
-            };
-
             if (!TratarErrosResponse(response))
             {
                 return new UsuarioRespostaLogin
                 {
-                    ResponseResult = JsonSerializer.Deserialize<ResponseResult>(await response.Content.ReadAsStringAsync(), options)
+                    ResponseResult = await DeserializarObjetoResponse<ResponseResult>(response)
 
                 };
             }
-
-            return JsonSerializer.Deserialize<UsuarioRespostaLogin>(await response.Content.ReadAsStringAsync());
+            return await DeserializarObjetoResponse<UsuarioRespostaLogin>(response);
         }
         public async Task<UsuarioRespostaLogin> Login(UsuarioLogin usuarioLogin)
         {
-            var loginContent = new StringContent(JsonSerializer.Serialize(usuarioLogin), Encoding.UTF8, "application/json");
+            var loginContent = ObterConteudo(usuarioLogin);
 
             var response = await _httpClient.PostAsync("https://localhost:44363/api/identidade/autenticar", loginContent);
-
-            var options = new JsonSerializerOptions
-            {
-                PropertyNameCaseInsensitive = true
-            };
 
             if (!TratarErrosResponse(response))
             {
                 return new UsuarioRespostaLogin
                 {
-                    ResponseResult = JsonSerializer.Deserialize<ResponseResult>(await response.Content.ReadAsStringAsync(), options)
+                    ResponseResult = await DeserializarObjetoResponse<ResponseResult>(response)
 
                 };
             }
 
-            return JsonSerializer.Deserialize<UsuarioRespostaLogin>(await response.Content.ReadAsStringAsync(), options);
+            return await DeserializarObjetoResponse<UsuarioRespostaLogin>(response);
         }
     }
 }
