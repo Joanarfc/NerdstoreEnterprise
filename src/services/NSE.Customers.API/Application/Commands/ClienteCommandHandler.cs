bc0fc67ae9efac9306a997c09bcd06e81c285953
Joanarfc <joanarfcarvalho@hotmail.com>
2023-02-16 16:05:34 -0300

    Added initial implementation of Command and CommandHandler in NSE.Customers.API

---
+++ src/services/NSE.Customers.API/Application/Commands/ClienteCommandHandler.cs
@@ -0,0 +1,13 @@
+﻿namespace NSE.Customers.API.Application.Commands
+{
+    public class ClienteCommandHandler
+    {
+        // Exemplo
+        public void Manipular(RegistarClienteCommand command)
+        {
+            // Validar command
+
+            // Persistir na base de dados
+        }
+    }
+}
