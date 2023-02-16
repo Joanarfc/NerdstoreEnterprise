7525a6a2be66390776d93f0653cd224eac785961
Joanarfc <joanarfcarvalho@hotmail.com>
2023-02-16 11:59:10 -0300

    Added extension class RazorHelpers and used its methods in the Catalogo views

---
+++ src/web/NSE.WebApp.MVC/Extensions/RazorHelpers.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+
+namespace NSE.WebApp.MVC.Extensions
+{
+    public static class RazorHelpers
+    {
+        public static string HashEmailForGravatar(this RazorPage page, string email)
+        {
+            /* Create an instance of the MD5 hash algorithm  */
+            var md5Hasher = MD5.Create();
+
+            /* Compute the hash value of the email by converting the email string to a byte array */
+            var data = md5Hasher.ComputeHash(Encoding.Default.GetBytes(email));
+
+            /* Create an empty StringBuilder object to store the hexadecimal representation of the hash value */
+            var sBuilder = new StringBuilder();
+
+            /* Iterate through the byte array "data" 
+             * and append each byte in hexadecimal format to the "sBuilder" object */
+            foreach (var t in data)
+            {
+                sBuilder.Append(t.ToString("x2"));
+            }
+
+            /* Return the string representation of the hash value */
+            return sBuilder.ToString();
+        }
+        public static string FormatoMoeda(this RazorPage page, decimal valor)
+        {
+            return valor > 0? string.Format(Thread.CurrentThread.CurrentCulture, "{0:C}", valor) : "Gratuito";
+        }
+        public static string MensagemStock(this RazorPage page, int quantidade)
+        {
+            return quantidade > 0 ? $"Apenas {quantidade} em stock!" : "Produto esgotado";
+        }
+    }
+}
