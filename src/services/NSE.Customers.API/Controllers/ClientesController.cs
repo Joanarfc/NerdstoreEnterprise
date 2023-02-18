a8ffdd704cab4ea6fd9aa6357c6ec340f2ed8070
Joanarfc <joanarfcarvalho@hotmail.com>
2023-02-17 21:31:35 -0300

    Added ClientesController and tested the data persistence in NSE.Customers.API

---
+++ src/services/NSE.Customers.API/Controllers/ClientesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NSE.Core.Mediator;
+using NSE.Customers.API.Application.Commands;
+using NSE.WebAPI.Core.Controllers;
+using System;
+using System.Threading.Tasks;
+
+namespace NSE.Customers.API.Controllers
+{
+    public class ClientesController : MainController
+    {
+        private readonly IMediatorHandler _mediatorHandler;
+
+        public ClientesController(IMediatorHandler mediatorHandler)
+        {
+            _mediatorHandler = mediatorHandler;
+        }
+
+        [HttpGet("clientes")]
+        public async Task<IActionResult> Index()
+        {
+            var resultado = await _mediatorHandler.EnviarComando(
+                new RegistarClienteCommand(Guid.NewGuid(), "Joana", "joana@teste.com", "21458053032"));
+
+            return CustomResponse(resultado);
+        }
+    }
+}
