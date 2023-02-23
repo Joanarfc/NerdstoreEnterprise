677fd6ded3a49c6100a697191e724a63d4f5c2bf
Joanarfc <joanarfcarvalho@hotmail.com>
2023-02-23 16:09:00 -0300

    Added basic CarrinhoController implementation to NSE.WebApp.MVC project

---
+++ src/web/NSE.WebApp.MVC/Controllers/CarrinhoController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NSE.WebApp.MVC.Models;
+using System.Threading.Tasks;
+using System;
+using Microsoft.AspNetCore.Authorization;
+
+namespace NSE.WebApp.MVC.Controllers
+{
+    [Authorize]
+    public class CarrinhoController : MainController
+    {
+        [Route("carrinho")]
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("carrinho/adicionar-item")]
+        public async Task<IActionResult> AdicionarItemCarrinho(ItemCarrinhoViewModel itemCarrinho)
+        {
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [Route("carrinho/atualizar-item")]
+        public async Task<IActionResult> AtualizarItemCarrinho(Guid produtoId, int quantidade)
+        {
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [Route("carrinho/remover-item")]
+        public async Task<IActionResult> RemoverItemCarrinho(Guid produtoId)
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
