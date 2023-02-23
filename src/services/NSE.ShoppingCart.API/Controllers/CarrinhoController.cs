c9e5d8f695f5c3613133e6daf1cc45ff7dc974cb
Joanarfc <joanarfcarvalho@hotmail.com>
2023-02-23 13:19:22 -0300

    Added basic CarrinhoController in NSE.ShoppingCart.API

---
+++ src/services/NSE.ShoppingCart.API/Controllers/CarrinhoController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NSE.ShoppingCart.API.Model;
+using NSE.WebAPI.Core.Controllers;
+using NSE.WebAPI.Core.Utilizador;
+using System;
+using System.Threading.Tasks;
+
+namespace NSE.ShoppingCart.API.Controllers
+{
+    [Authorize]
+    public class CarrinhoController : MainController
+    {
+        private readonly IAspNetUser _user;
+
+        public CarrinhoController(IAspNetUser user)
+        {
+            _user = user;
+        }
+
+        [HttpGet("carrinho")]
+        public async Task<CarrinhoCliente> ObterCarrinho()
+        {
+            return null;
+        }
+
+        [HttpPost("carrinho")]
+        public async Task<IActionResult> AdicionarItemCarrinho(CarrinhoItem item)
+        {
+            return CustomResponse();
+        }
+
+        [HttpPut("carrinho/{produtoId}")]
+        public async Task<IActionResult> AtualizarItemCarrinho(Guid produtoId, CarrinhoItem item)
+        {
+            return CustomResponse();
+        }
+
+        [HttpDelete("carrinho/{produtoId}")]
+        public async Task<IActionResult> RemoverItemCarrinho(Guid produtoId)
+        {
+            return CustomResponse();
+        }
+    }
+}
