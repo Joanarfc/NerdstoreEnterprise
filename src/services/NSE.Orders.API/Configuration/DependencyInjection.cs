6db1ef9c109bb759206633d83d071e94637093f4
Joanarfc <joanarfcarvalho@hotmail.com>
2023-02-25 18:00:59 -0300

    Added initial configurations to NSE.Orders.API

---
+++ src/services/NSE.Orders.API/Configuration/DependencyInjection.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace NSE.Orders.API.Configuration
+{
+    public static class DependencyInjectionConfig
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+        }
+    }
+}
