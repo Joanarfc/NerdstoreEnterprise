68e6dd7e8fb6c0457d29dc9399cfde4cf05b9311
Joanarfc <joanarfcarvalho@hotmail.com>
2023-02-27 18:12:36 -0300

    Added infrastructure layer for Pedido in NSE.Orders.Infra

---
+++ src/services/NSE.Orders.Infra/Data/Mappings/PedidoItemMapping.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using NSE.Orders.Domain.Pedidos;
+
+namespace NSE.Orders.Infra.Data.Mappings
+{
+    public class PedidoItemMapping : IEntityTypeConfiguration<PedidoItem>
+    {
+        public void Configure(EntityTypeBuilder<PedidoItem> builder)
+        {
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.ProdutoNome)
+                .IsRequired()
+                .HasColumnType("varchar(250)");
+
+            // 1 : N => Pedido : PedidoItens
+            builder.HasOne(c => c.Pedido)
+                .WithMany(c => c.PedidoItens);
+
+            builder.ToTable("PedidoItens");
+        }
+    }
+}
