e92e0e02117d9c6211ffb27bb5b33f7bd49beee8
Joanarfc <joanarfcarvalho@hotmail.com>
2023-02-27 13:13:35 -0300

    Added Infra layer, NSE.Orders.Infra, with PedidosContext and VoucherRepository class

---
+++ src/services/NSE.Orders.Infra/Data/Repository/VoucherRepository.cs
@@ -0,0 +1,21 @@
+﻿using NSE.Core.Data;
+using NSE.Orders.Domain.Vouchers;
+
+namespace NSE.Orders.Infra.Data.Repository
+{
+    public class VoucherRepository : IVoucherRepository
+    {
+        private readonly PedidosContext _context;
+
+        public VoucherRepository(PedidosContext context)
+        {
+            _context = context;
+        }
+        public IUnitOfWork UnitOfWork => _context;
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
