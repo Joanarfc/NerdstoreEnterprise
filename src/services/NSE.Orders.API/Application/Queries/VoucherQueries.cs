af96de2274c19686f65651ca4f016afa11f804cf
Joanarfc <joanarfcarvalho@hotmail.com>
2023-02-27 14:32:21 -0300

    Added VoucherQueries in NSE.Orders.API and implemented the Specification Pattern in NSE.Orders.Domain

---
+++ src/services/NSE.Orders.API/Application/Queries/VoucherQueries.cs
@@ -0,0 +1,38 @@
+﻿using NSE.Orders.API.Application.DTO;
+using NSE.Orders.Domain.Vouchers;
+using System.Threading.Tasks;
+
+namespace NSE.Orders.API.Application.Queries
+{
+    public interface IVoucherQueries
+    {
+        Task<VoucherDTO> ObterVoucherPorCodigo(string codigo);
+    }
+    public class VoucherQueries : IVoucherQueries
+    {
+        private readonly IVoucherRepository _voucherRepository;
+
+        public VoucherQueries(IVoucherRepository voucherRepository)
+        {
+            _voucherRepository = voucherRepository;
+        }
+
+        public async Task<VoucherDTO> ObterVoucherPorCodigo(string codigo)
+        {
+            var voucher = await _voucherRepository.ObterVoucherPorCodigo(codigo);
+
+            if (voucher == null) return null;
+
+            // Verify if the voucher is valid
+            if (!voucher.EstaValidoParaUtilizacao()) return null;
+
+            return new VoucherDTO
+            {
+                Codigo = voucher.Codigo,
+                TipoDesconto = (int)voucher.TipoDesconto,
+                Percentual = voucher.Percentual,
+                ValorDesconto = voucher.ValorDesconto
+            };
+        }
+    }
+}
