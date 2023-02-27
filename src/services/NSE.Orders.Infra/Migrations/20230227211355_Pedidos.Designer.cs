63c38bda75509cbd1ad597f8bb88333a0885e116
Joanarfc <joanarfcarvalho@hotmail.com>
2023-02-27 18:15:50 -0300

    Added migration Pedidos for PedidosContext in NSE.Orders.Infra

--- src/services/NSE.Orders.Infra/Migrations/20230227211355_Pedidos.Designer.cs
+++ src/services/NSE.Orders.Infra/Migrations/20230227211355_Pedidos.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NSE.Orders.Infra.Data;
 
 namespace NSE.Orders.Infra.Migrations
 {
     [DbContext(typeof(PedidosContext))]
-    partial class PedidosContextModelSnapshot : ModelSnapshot
+    [Migration("20230227211355_Pedidos")]
+    partial class Pedidos
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
