841cb930874e8b5d65c43447c8be61bf41e30b0e
Joanarfc <joanarfcarvalho@hotmail.com>
2023-02-27 14:57:58 -0300

    Added Voucher implementation in NSE.ShoppingCart.API

--- src/services/NSE.ShoppingCart.API/Model/CarrinhoCliente.cs
+++ src/services/NSE.ShoppingCart.API/Model/CarrinhoCliente.cs
@@ -14,15 +14,52 @@ namespace NSE.ShoppingCart.API.Model
         public decimal ValorTotal { get; set; }
         public List<CarrinhoItem> Itens { get; set; } = new List<CarrinhoItem>();
         public ValidationResult ValidationResult { get; set; }
+        public bool VoucherUtilizado { get; set; }
+        public decimal Desconto { get; set; }
+        public Voucher Voucher { get; set; }
         public CarrinhoCliente(Guid clienteId)
         {
             Id = Guid.NewGuid();
             ClienteId = clienteId;
         }
         public CarrinhoCliente() { }
+        public void AplicarVoucher(Voucher voucher)
+        {
+            Voucher = voucher;
+            VoucherUtilizado = true;
+            CalcularValorCarrinho();
+        }
         internal void CalcularValorCarrinho()
         {
             ValorTotal = Itens.Sum(p => p.CalcularValor());
+            CalcularValorTotalDesconto();
+        }
+        private void CalcularValorTotalDesconto()
+        {
+            if (!VoucherUtilizado) return;
+
+            decimal desconto = 0;
+            var valor = ValorTotal;
+
+            if (Voucher.TipoDesconto == TipoDescontoVoucher.Percentagem)
+            {
+                if (Voucher.Percentual.HasValue)
+                {
+                    desconto = (valor * Voucher.Percentual.Value) / 100;
+                    valor -= desconto;
+                }
+            }
+            else
+            {
+                if (Voucher.ValorDesconto.HasValue)
+                {
+                    desconto = Voucher.ValorDesconto.Value;
+                    valor -= desconto;
+                }
+            }
+
+            ValorTotal = valor < 0 ? 0 : valor;
+            Desconto = desconto;
         }
         internal bool CarrinhoItemExistente(CarrinhoItem item)
         {
---
+++ src/services/NSE.ShoppingCart.API/Model/Voucher.cs
@@ -0,0 +1,16 @@
+﻿namespace NSE.ShoppingCart.API.Model
+{
+    public class Voucher
+    {
+        public decimal? Percentual { get; set; }
+        public decimal? ValorDesconto { get; set; }
+        public string Codigo { get; set; }
+        public TipoDescontoVoucher TipoDesconto { get; set; }
+    }
+
+    public enum TipoDescontoVoucher
+    {
+        Percentagem = 0,
+        Valor = 1
+    }
+}
