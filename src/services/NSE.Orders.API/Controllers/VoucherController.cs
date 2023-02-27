84f28a020ce870c9b959844508cfd73e902df288
Joanarfc <joanarfcarvalho@hotmail.com>
2023-02-27 13:48:56 -0300

    Added VoucherController to NSE.Orders.API

---
+++ src/services/NSE.Orders.API/Controllers/VoucherController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using NSE.WebAPI.Core.Controllers;
+
+namespace NSE.Orders.API.Controllers
+{
+    [Authorize]
+    public class VoucherController : MainController
+    {
+    }
+}
