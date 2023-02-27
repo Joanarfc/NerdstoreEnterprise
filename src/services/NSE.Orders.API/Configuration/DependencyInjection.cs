157e9193850a087d07d5dc4ee7673ec016501afd
Joanarfc <joanarfcarvalho@hotmail.com>
2023-02-27 14:39:24 -0300

    Added action ObterPorCodigo to VoucherController and registered the services dependency in NSE.Orders.API

--- src/services/NSE.Orders.API/Configuration/DependencyInjection.cs
+++ src/services/NSE.Orders.API/Configuration/DependencyInjection.cs
@@ -1,6 +1,11 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
 using NSE.Core.Mediator;
+using NSE.Orders.API.Application.Queries;
+using NSE.Orders.Domain.Vouchers;
 using NSE.Orders.Infra.Data;
+using NSE.Orders.Infra.Data.Repository;
+using NSE.WebAPI.Core.Utilizador;
 
 namespace NSE.Orders.API.Configuration
 {
@@ -8,8 +13,17 @@ namespace NSE.Orders.API.Configuration
     {
         public static void RegisterServices(this IServiceCollection services)
         {
-            services.AddScoped<PedidosContext>();
+            //API
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddScoped<IAspNetUser, AspNetUser>();
+
+            //Application
             services.AddScoped<IMediatorHandler, MediatorHandler>();
+            services.AddScoped<IVoucherQueries, VoucherQueries>();
+
+            //Data
+            services.AddScoped<PedidosContext>();
+            services.AddScoped<IVoucherRepository, VoucherRepository>();
         }
     }
 }
