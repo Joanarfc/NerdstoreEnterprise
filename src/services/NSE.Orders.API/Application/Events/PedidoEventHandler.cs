c8a5f0b37ac3b272788906ab776ee7316bee3ee2
Joanarfc <joanarfcarvalho@hotmail.com>
2023-02-28 15:48:17 -0300

    Shopping cart manipulation in NSE.Orders.API: added PedidoEventHandler configuration

---
+++ src/services/NSE.Orders.API/Application/Events/PedidoEventHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using NSE.Core.Messages.Integration;
+using NSE.MessageBus;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace NSE.Orders.API.Application.Events
+{
+    public class PedidoEventHandler : INotificationHandler<PedidoRealizadoEvent>
+    {
+        private readonly IMessageBus _bus;
+        public PedidoEventHandler(IMessageBus bus)
+        {
+            _bus = bus;
+        }
+        public async Task Handle(PedidoRealizadoEvent message, CancellationToken cancellationToken)
+        {
+            await _bus.PublishAsync(new PedidoRealizadoIntegrationEvent(message.ClienteId));
+        }
+    }
+}
