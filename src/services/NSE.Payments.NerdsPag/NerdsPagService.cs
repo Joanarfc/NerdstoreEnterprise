5454186637c46b55de84ed0c9d1119fa87894da2
Joanarfc <joanarfcarvalho@hotmail.com>
2023-03-02 11:29:36 -0300

    Added entities structure of the payment gateway in NSE.Payments.NerdsPag

---
+++ src/services/NSE.Payments.NerdsPag/NerdsPagService.cs
@@ -0,0 +1,14 @@
+﻿namespace NSE.Payments.NerdsPag
+{
+    public class NerdsPagService
+    {
+        public readonly string ApiKey;
+        public readonly string EncryptionKey;
+
+        public NerdsPagService(string apiKey, string encryptionKey)
+        {
+            ApiKey = apiKey;
+            EncryptionKey = encryptionKey;
+        }
+    }
+}
---
+++ src/services/NSE.Payments.NerdsPag/PaymentMethod.cs
@@ -0,0 +1,9 @@
+﻿
+namespace NSE.Payments.NerdsPag
+{
+    public enum PaymentMethod
+    {
+        CreditCard = 1,
+        Billet
+    }
+}
