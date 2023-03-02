af294659b8dea7e12584451514f767d5251b565b
Joanarfc <joanarfcarvalho@hotmail.com>
2023-03-02 14:19:05 -0300

    Added Payment background service configuration in NSE.Payments.API

---
+++ src/services/NSE.Payment.API/Services/IPagamentoService.cs
@@ -0,0 +1,11 @@
+﻿using NSE.Payments.API.Models;
+using System.Threading.Tasks;
+using NSE.Core.Messages.Integration;
+
+namespace NSE.Payments.API.Services
+{
+    public interface IPagamentoService
+    {
+        Task<ResponseMessage> AutorizarPagamento(Pagamento pagamento);
+    }
+}
