7fd379247b587a92c772a478871b8765a918f153
Joanarfc <joanarfcarvalho@hotmail.com>
2023-03-02 11:21:56 -0300

    Renamed project to "NSE.Payments.API" and added initial configurations

---
+++ src/services/NSE.Payment.API/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace NSE.Payments.API.Configuration
+{
+    public static class DependencyInjectionConfig
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+        }
+    }
+}
