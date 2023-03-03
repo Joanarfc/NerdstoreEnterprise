a31104d9f6e1814bba0a6a251d39976ae0aa9b09
Joanarfc <joanarfcarvalho@hotmail.com>
2023-03-03 11:35:30 -0300

    Added implementation to Capture and Cancel a payment in NSE.Payments.API

--- src/services/NSE.Payment.API/Data/Repository/PagamentoRepository.cs
+++ src/services/NSE.Payment.API/Data/Repository/PagamentoRepository.cs
@@ -1,5 +1,10 @@
 ﻿using NSE.Payments.API.Models;
 using NSE.Core.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System;
+using System.Linq;
 
 namespace NSE.Payments.API.Data.Repository
 {
@@ -18,7 +23,21 @@ namespace NSE.Payments.API.Data.Repository
         {
             _context.Pagamentos.Add(pagamento);
         }
+        public void AdicionarTransacao(Transacao transacao)
+        {
+            _context.Transacoes.Add(transacao);
+        }
+        public async Task<Pagamento> ObterPagamentoPorPedidoId(Guid pedidoId)
+        {
+            return await _context.Pagamentos.AsNoTracking()
+                .FirstOrDefaultAsync(p => p.PedidoId == pedidoId);
+        }
 
+        public async Task<IEnumerable<Transacao>> ObterTransacaoesPorPedidoId(Guid pedidoId)
+        {
+            return await _context.Transacoes.AsNoTracking()
+                .Where(t => t.Pagamento.PedidoId == pedidoId).ToListAsync();
+        }
         public void Dispose()
         {
             _context.Dispose();
--- src/services/NSE.Payment.API/Services/IPagamentoService.cs
+++ src/services/NSE.Payment.API/Services/IPagamentoService.cs
@@ -1,11 +1,14 @@
 ﻿using NSE.Payments.API.Models;
 using System.Threading.Tasks;
 using NSE.Core.Messages.Integration;
+using System;
 
 namespace NSE.Payments.API.Services
 {
     public interface IPagamentoService
     {
         Task<ResponseMessage> AutorizarPagamento(Pagamento pagamento);
+        Task<ResponseMessage> CapturarPagamento(Guid pedidoId);
+        Task<ResponseMessage> CancelarPagamento(Guid pedidoId);
     }
 }
