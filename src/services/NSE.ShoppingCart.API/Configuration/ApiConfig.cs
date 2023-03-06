afd05f4074c7a217ce73616e23c8909170638b81
Joanarfc <joanarfcarvalho@hotmail.com>
2023-03-06 12:43:09 -0300

    Added gRPC Server implementation in NSE.ShoppingCart.API

--- src/services/NSE.ShoppingCart.API/Configuration/ApiConfig.cs
+++ src/services/NSE.ShoppingCart.API/Configuration/ApiConfig.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using NSE.ShoppingCart.API.Data;
+using NSE.ShoppingCart.API.Services.gRPC;
 using NSE.WebAPI.Core.Identidade;
 
 namespace NSE.ShoppingCart.API.Configuration
@@ -17,6 +18,8 @@ namespace NSE.ShoppingCart.API.Configuration
             options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
 
             services.AddControllers();
+
+            services.AddGrpc();
 
             services.AddCors(options =>
             {
@@ -45,6 +48,7 @@ namespace NSE.ShoppingCart.API.Configuration
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapGrpcService<CarrinhoGrpcService>().RequireCors("Total");
             });
         }
     }
