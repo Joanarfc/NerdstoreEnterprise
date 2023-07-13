d7dc7c6b30f4c66c76d1116d823b74771bbbadbc
Joana Carvalho <36134456+Joanarfc@users.noreply.github.com>
2023-07-13 03:31:48 -0300

    Migrated NSE.WebApp.MVC from .NET Core 3.1 to .NET 6.0

--- src/web/NSE.WebApp.MVC/Program.cs
+++ src/web/NSE.WebApp.MVC/Program.cs
@@ -1,20 +1,26 @@
-using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using NSE.WebApp.MVC.Configuration;
 
-namespace NSE.WebApp.MVC
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json", true, true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true)
+    .AddEnvironmentVariables();
+
+if (builder.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+    builder.Configuration.AddUserSecrets<Program>();
 }
+
+builder.Services.AddAuthenticationConfiguration();
+builder.Services.AddMvcConfiguration(builder.Configuration);
+builder.Services.RegisterServices(builder.Configuration);
+
+var app = builder.Build();
+
+app.UseMvcConfiguration(app.Environment);
+
+app.Run();
